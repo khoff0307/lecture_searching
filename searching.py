a48fa10b31a199055b01ab0ebad40946bd3f075c
Katerina Hoffmannova <k.hoffmann1907@gmail.com>
2024-04-09 09:35:28 +0200

    Searching

--- searching.py
+++ searching.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 # get current working directory path
 cwd_path = os.getcwd()
 
@@ -13,9 +13,43 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    json_filename = file_name
+    with open(json_filename, "r") as file_obj:
+        data = json.load(file_obj)
+    sequence = data[field]
+    return sequence
+
+def search_number(sequence, number):
+    """
+    Search for position of number in sequence
+    :param sequence: [list] list of numbers
+    :param number: [int] searched number
+    :return: [int] position in list
+    """
+    i = 0
+    dict = {}
+    positions = []
+    count = 0
+    while i < len(sequence):
+        if sequence[i] == number:
+            print(f"Hledané číslo {number} se nachází na pozici {i}.")
+            positions.append(i)
+            count += 1
+            dict["Positions:"] = positions
+            dict["Count:"] = count
+            i += 1
+        else:
+            i += 1
+            continue
+    if count != 0:
+        return dict
+    return print(f"Hledané číslo {number} není v seznamu.")
+
 
 def main():
-    1+1
+    sequence = read_data("sequential.json", "unordered_numbers")
+    print(search_number(sequence, 0))
+
     pass
 
 
